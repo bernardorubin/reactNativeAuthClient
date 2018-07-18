e2a8a0cae8213ccd0781a7d9d91469c6f13bfa99
Bernardo Emilio Rubin de Celis Martinez <bernulander@gmail.com>
2018-07-17 20:28:50 -0500

    Created Auth Screen

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { Navigation } from 'react-native-navigation';
+import AuthScreen from './Screens/AuthScreen';
+
+Navigation.registerComponent('client.AuthScreen' () => AuthScreen);
+
+Navigation.startSingleScreenApp({
+  screen: {
+    screen: "client.AuthScreen"
+  }
+})
