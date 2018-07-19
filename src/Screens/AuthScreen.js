59f619ddcffca4349ae0eca90117a9abcd24e91f
Bernardo Emilio Rubin de Celis Martinez <bernulander@gmail.com>
2018-07-18 19:59:50 -0500

    Added Text Input logic

--- src/Screens/AuthScreen.js
+++ src/Screens/AuthScreen.js
@@ -9,14 +9,32 @@ export default class App extends Component<Props> {
   static navigatorStyle = {
     navBarHidden: true,
   }
+  state = {
+    username: '',
+    password: '',
+  }
+  handleChangeUsername = text => {
+    // this.setState({ username: text });
+    this.setState(() => {
+      return { username: text }
+    }, () => {
+      //setstate is asynchronous, it can receive this callback
+      console.log(this.state.username)
+    })
+  }
+  handleChangePassword = text => {
+    this.setState(() => {
+      return { password: text }
+    })
+  }
   render() {
     return <View style={styles.container}>
         <Image source={require("../../assets/images/logo.png")} style={styles.image} />
         <View style={styles.formContainer}>
-          <Input placeholder="Username" />
-          <Input placeholder="Password" secureTextEntry/>
+          <Input placeholder="Username" onChangeText={this.handleChangeUsername} value={this.state.username} />
+          <Input placeholder="Password" secureTextEntry onChangeText={this.handleChangePassword} value={this.state.password}/>
         </View>
-        <View style={{ alignItems: 'center', height: 150, justifyContent: 'space-around'}}>
+        <View style={{ alignItems: "center", height: 150, justifyContent: "space-around" }}>
           <CustomButton />
           <TextButton />
         </View>
