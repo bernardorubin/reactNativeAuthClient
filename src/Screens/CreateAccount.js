51d75cbf71fc94992037cb8dc205c5f7e26496a6
Bernardo Emilio Rubin de Celis Martinez <bernulander@gmail.com>
2018-07-19 01:11:45 -0500

    Initialized state and connected state to inputs on create account

--- src/Screens/CreateAccount.js
+++ src/Screens/CreateAccount.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import Validator from 'validator';
 import Input from '../Components/Input/Input';
 import CustomButton from '../Components/Button/Button';
 
 class CreateAccount extends Component {
-  state = {};
+  state = {
+    email: '',
+    password: '',
+    username: '',
+  };
+  handleEmailChange = email => this.setState({ email });
+  handleUsernameChange = username => this.setState({ username });
+  handlePasswordChange = password => this.setState({ password });
   render() {
     const { container, signUpForm } = styles;
     return (
       <View style={container}>
         <View style={signUpForm}>
-          <Input placeholder="Email"/>
-          <Input placeholder="Username"/>
-          <Input placeholder="Password"/>
+          <Input placeholder="Email" value={this.state.email} onChangeText={this.handleEmailChange}/>
+          <Input placeholder="Username" value={this.state.username} onChangeText={this.handleUsernameChange}/>
+          <Input placeholder="Password" value={this.state.password} onChangeText={this.handlePasswordChange}/>
         </View>
-        <CustomButton text="Sign Up"/>
+        <CustomButton text="Sign Up" />
       </View>
     );
   }
